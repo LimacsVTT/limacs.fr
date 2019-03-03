c1503d1d3b377b84138b55fd3d8b4c7813abfc50
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-03-03 17:24:38 +0100

    Disable CSS inlining in production builds

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onPreRenderHTML = ({ getHeadComponents }) => {
+  if (process.env.NODE_ENV !== 'production') { return; }
+
+  const headComponents = getHeadComponents();
+  headComponents.forEach(el => {
+    if (el.type === 'style') {
+      /* eslint-disable no-param-reassign */
+      el.type = 'link';
+      el.props.href = el.props['data-href'];
+      el.props.rel = 'stylesheet';
+      el.props.type = 'text/css';
+
+      delete el.props['data-href'];
+      delete el.props.dangerouslySetInnerHTML;
+      /* eslint-enable */
+    }
+  });
+};
